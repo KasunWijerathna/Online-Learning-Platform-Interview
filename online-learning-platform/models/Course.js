53684c697417c0ebb54bd5cecf51eb627345a358
KasunWijerathna <kasun.2019266@iit.ac.lk>
2024-06-17 21:27:55 +0530

    do minor changes

--- online-learning-platform/models/Course.js
+++ online-learning-platform/models/Course.js
@@ -2,6 +2,7 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const Course = sequelize.define('Course', {
+  //Todo:add image and other course details to end point
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   title: { type: DataTypes.STRING, allowNull: false },
   description: { type: DataTypes.TEXT, allowNull: false }
